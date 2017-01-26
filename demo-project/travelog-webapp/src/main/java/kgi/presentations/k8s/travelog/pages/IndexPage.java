bb8e4ec9431920784da73df81ee5a6bbcf93080a
ignak004 <Konstantin.X.Ignatyev.-ND@disney.com>
2017-01-25 19:35:37 -0800

    adding thymeleaf templating;

---
+++ demo-project/travelog-webapp/src/main/java/kgi/presentations/k8s/travelog/pages/IndexPage.java
@@ -0,0 +1,31 @@
+package kgi.presentations.k8s.travelog.pages;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kgi.presentations.k8s.common.TravelConfigProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+
+@Controller
+public class IndexPage {
+
+
+    @Resource
+    TravelConfigProperties configProperties;
+
+
+    @Resource
+    ObjectMapper om;
+
+
+    @GetMapping(path = {"/"})
+    String index(Model model) {
+        model.addAttribute("initScript", " travelog ={}; " +
+                "travelog.in_bucket = '" + configProperties.input_bucket + "';" +
+                "travelog.access_key_id = '" + configProperties.access_key_id + "';");
+        return "index";
+    }
+}
